7e6ada289f7da5ab2439c9b7b63ab55e569167ec
Priyankaabuj <it.a.01.priyanka.abuj@gmail.com>
2022-11-10 08:30:27 +0530

    added file

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum
+{
+public static void main(string args[])
+{
+int a,b;
+a=Integer.parseInt(args[0]);
+a=Integer.parseInt(args[1]);
+System.out.println("Addition is: "+(a+b));
+}
+}
